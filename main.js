503ee417fe0d81f6461063bbba8e8260d529b1f4
Islom Alimov <islomalimovm@gmail.com>
2020-01-25 19:54:47 +0100

    for loop in Vue

--- main.js
+++ main.js
@@ -2,7 +2,29 @@ var app = new Vue({
     el: '#app',
     data: {
         product: 'Socks',
-        image: 'vmSocks-green-onWhite.jpg'
+        image: 'vmSocks-green-onWhite.jpg',
+        inventory: 11,
+        detials: ["80% cotton", "20% polyester", "Gender-neutral"],
+        variants: [
+            {
+                variantID: 2234,
+                variantColor: "green"
+
+            },
+            {
+                variantID: 2235,
+                variantColor: "blue"
+
+            }
+        ],
+        cart:0,
+        methods: {
+            addToCart() {
+                this.cart += 1
+                console.log("nima")
+            },
+        },
+
     }
 }
 )
